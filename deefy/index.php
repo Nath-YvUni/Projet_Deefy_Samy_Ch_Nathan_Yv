398b38e3900d076f6d7692508e05ba8d6941802d
Nathan Yvon <nathan.yvon5@etu.univ-lorraine.fr>
2025-10-28 23:53:14 +0100

    Index Modifié et ajout des images d'avatar fait par les utilisateurs

--- deefy/index.php
+++ deefy/index.php
@@ -2,13 +2,8 @@
 // index.php
 session_start();
 
-require_once 'classes/BDD/Database.php';
-
-use Deefy\BDD\Database;
-
-// On initialise la base avec le fichier .ini
-Database::setConfigFile(__DIR__ . '/ressources/acces/db.ini');
-$pdo = Database::getConnection();
+// Inclure l'init pour récupérer $pdo
+require_once 'classes/init.php';
 
 // Vérifier si l'utilisateur est connecté
 $estConnecte = isset($_SESSION['user']);
@@ -72,13 +67,13 @@ $musics = $stmt2->fetchAll(PDO::FETCH_ASSOC);
       <!-- MENU DÉROULANT -->
       <div class="dropdown-menu" id="dropdownMenu">
         <!-- TODO : Création d'une page profil et d'un lien vers une page pour ce login et s'inscrire mais qui peut aussi servir a se déco -->
-        <a href="classes/profile.php">Mon profil</a>
+        <a href="Fonctionnalité/profile.php">Mon profil</a>
         <hr>
-        <a href="classes/Log_Sig.php?action=logout" class="logout">Se déconnecter</a>
+        <a href="Fonctionnalité/Log_Sig.php?action=logout" class="logout">Se déconnecter</a>
       </div>
     </div>
     <?php else: ?>
-      <a href="classes/Log_Sig.php" class="login">Se connecter / S'inscrire</a>
+      <a href="Fonctionnalité/Log_Sig.php" class="login">Se connecter / S'inscrire</a>
     <?php endif; ?>
   </header>
 
